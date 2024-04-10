09153865ddf12faa66f83064c2439845adcede3d
HTY <1044213317@qq.com>
2024-04-10 13:19:40 +0800

    feat: add bash param

--- eval_humanoid.sh
+++ eval_humanoid.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
 env="Humanoid-v4"
-algo="ppo"
 log_folder=./logs/
 exp_id=0
 
-export PYTHONPATH=./:$PYTHONPATH
-python ./scripts/eval.py --env ${env} --algo ${algo} -f ${log_folder} --exp-id ${exp_id} --load-best
+eval()
+{
+    algo="${1:-sac}"
+    export PYTHONPATH=./:$PYTHONPATH
+    python ./scripts/eval.py --env ${env} --algo ${algo} -f ${log_folder} --exp-id ${exp_id} --load-best
+}
 
+eval "$@"
 
--- train_humanoid.sh
+++ train_humanoid.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 env="Humanoid-v4"
-algo="trpo"
 save_freq=200000
 num_threads=8
 seed=1
 
-echo "env is ${env}, algo is ${algo}, seed is ${seed}"
-export PYTHONPATH=./:$PYTHONPATH
-python ./scripts/train.py --env ${env} --algo ${algo} --save-freq ${save_freq} \
---num-threads ${num_threads} --seed ${seed} --track
+train()
+{
+    algo="${1:-sac}"
+    echo "env is ${env}, algo is ${algo}, seed is ${seed}"
+    export PYTHONPATH=./:$PYTHONPATH
+    python ./scripts/train.py --env ${env} --algo ${algo} --save-freq ${save_freq} \
+    --num-threads ${num_threads} --seed ${seed} --track
+}
 
-
+train "$@"
